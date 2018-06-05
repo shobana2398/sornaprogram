be6a519ab918225ada8b9f068a6f84bbde354093
shobana2398 <39941311+shobana2398@users.noreply.github.com>
2018-06-05 13:26:20 +0530

    Update and rename evenodd.py to vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+let=raw_input("Enter your character")
+if(let=='a' or let=='i' or let=='e' or let=='o' or let=='u' or let=='A' or let=='E' or let=='I' or let=='O'  or let=='U'):
+	print("The letter is vowel")
+else:
+	print("The letter is consonent")
