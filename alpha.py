c0bad12df25352a17a11de4b8cfb34a1d848fe87
shobana2398 <39941311+shobana2398@users.noreply.github.com>
2018-06-05 14:57:12 +0530

    Update and rename alpha.py to larger.py

--- alpha.py
+++
@@ -1,5 +0,0 @@
-let=raw_input("Enter the character")
-if(let>='a' and let<='z'or let>='A' and let<='Z'):
-	print("It is an alphabet")
-else:
-	print("It is not an alphabet")
---
+++ larger.py
@@ -0,0 +1,3 @@
+x,y,z=raw_input("Enter the three numbers").split(',')
+c=max(x,y,z)
+print('The largest number is',c)
