46bfdcd277c87bc2c457f7340a6e6a888a6d8fc9
shobana2398 <39941311+shobana2398@users.noreply.github.com>
2018-06-05 12:26:31 +0530

    Create posineg.py

---
+++ posineg.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number"))
+if(num<0):
+	print("\nnegative number")
+elif(num==0):
+	print("\nthe number is zero")
+elif(num>0):
+	print("\nThe number is positive number")
+else:
+	print("\nEnter the valid number")
