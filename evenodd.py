f584d2b957aff4e27cf4724d01964db06735dca5
shobana2398 <39941311+shobana2398@users.noreply.github.com>
2018-06-05 13:11:32 +0530

    Update and rename posineg.py to evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number"))
+if(num<=10000):
+	if(num%2==0):
+		print("\n the number even number")
+	
+	else:
+		print("The number  is odd")
+else:
+	print("The number is above 10000 please enter  less than 10000")
