63c0d93afb76b43feb65bcb1600bf90f261b0f72
shobana2398 <39941311+shobana2398@users.noreply.github.com>
2018-06-05 12:50:12 +0530

    Update posineg.py

--- posineg.py
+++ posineg.py
@@ -1,9 +1,12 @@
 num=int(input("Enter the number"))
-if(num<0):
-	print("\nnegative number")
-elif(num==0):
-	print("\nthe number is zero")
-elif(num>0):
-	print("\nThe number is positive number")
+if(num<=10000):
+	if(num<0):
+		print("\nthat is negative number")
+	elif(num==0):
+		print("\nthe number is zero")
+	elif(num>0):
+		print("\nThe number is positive number")
+	else:
+		printf("The correctnumber")
 else:
-	print("\nEnter the valid number")
+	print("The number is above 10000 please enter  less than 10000")
